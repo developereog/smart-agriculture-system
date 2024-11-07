6c650d8e00ad13ce70d914cfef02a906424c08b7
developereog <emmanuelgor2138@gmail.com>
2024-11-07 21:06:09 +0300

    Added project files for Smart Agriculture System

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// src/index.ts
+console.log("Welcome to the Smart Agriculture System!");
---
+++ tests/index.test.ts
@@ -0,0 +1,4 @@
+// tests/index.test.ts
+test("Welcome message test", () => {
+    expect("Welcome to the Smart Agriculture System!").toBe("Welcome to the Smart Agriculture System!");
+});
